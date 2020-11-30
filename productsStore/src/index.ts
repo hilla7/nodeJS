bb562fa13edb6d5306f609bd80734d2cdc35de81
Hilla <hillan@codevalue.net>
2020-11-30 08:59:22 +0200

    dotenv integrated

--- productsStore/src/index.ts
+++ productsStore/src/index.ts
@@ -1,6 +1,6 @@
 import { app } from './app';
 
-app.set('port', process.env.PORT || 8000);
+app.set('port', process.env.APP_PORT || 8001);
 
 app.listen(app.get('port'), () => {
   console.log('Application is running at http://localhost:%d in %s mode', app.get('port'), app.get('env'));
